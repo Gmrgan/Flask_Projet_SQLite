82c5b57db656b1bbb973909b7744bc0250eb1ced
Gmrgan <115547191+Gmrgan@users.noreply.github.com>
2025-03-18 14:43:40 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,6 +12,10 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 def est_authentifie():
     return session.get('authentifie')
 
+# Vérifier si l'utilisateur est un "user" (et non admin)
+def est_utilisateur():
+    return session.get('username') == 'user'
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
